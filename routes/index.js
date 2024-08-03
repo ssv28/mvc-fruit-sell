548d98c08624b21d4b4bac3886fd4ade4a095d6e
Your Name <you@example.com>
2024-08-03 18:04:26 +0530

    add

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let router = express.Router();
+let fs = require('fs')
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  let values = fs.readFileSync('card-detail/json/data.json')
+  const jsonData = JSON.parse(values);
+  console.log(jsonData);
+  res.render('index', { values: jsonData })
+});
+
+router.get('/product', function(req, res) {
+  let values = fs.readFileSync('card-detail/json/data.json')
+
+    console.log(req.query.id)
+
+    let id = req.query.id
+
+    const jsonData = JSON.parse(values);
+    console.log(jsonData[id].productPrise);
+    console.log(jsonData)
+    res.render('product', { values: jsonData[id] })
+})
+
+module.exports = router;
