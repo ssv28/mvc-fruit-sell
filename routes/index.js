3818adb47de66bd49df5e996973d3104ca4a9f14
Your Name <you@example.com>
2024-08-03 18:04:51 +0530

    add

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 let fs = require('fs')
 
+
 /* GET home page. */
 router.get('/', function(req, res) {
   let values = fs.readFileSync('card-detail/json/data.json')
